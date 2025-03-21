525edfedd7526e159af2d83aee53182165e0a75c
Md Al Hossain Mukib <143283871+mdmokib@users.noreply.github.com>
2025-03-21 11:19:00 +0600

    Add files via upload

---
+++ day1project.py
@@ -0,0 +1,8 @@
+print("Hello!")
+
+city_name=input("Where do you live in?\n")
+pet_name=input("What is the name of your pet?\n")
+
+print(city_name)
+print(pet_name)
+print("Your band name could be "+city_name+" "+pet_name)
